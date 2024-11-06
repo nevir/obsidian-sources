55e452a632241e6b879356d8322b3d7e421a9e8b
Ian MacLeod <ian@nevir.net>
2024-11-06 14:29:14 -0800

    Set up solid & macaron

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,33 @@
 import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+import { macaronVitePlugin } from '@macaron-css/vite';
 
+// https://vite.dev/config/
 export default defineConfig({
+  plugins: [macaronVitePlugin(), solidPlugin()],
+
+  // https://vite.dev/config/build-options.html
   build: {
     outDir: '.',
     target: 'node18',
     sourcemap: 'inline',
+
+    // https://vite.dev/guide/build#library-mode
     lib: {
       entry: 'src/main.ts',
       formats: ['cjs'],
       fileName: () => `main.js`,
     },
+
+    // https://rollupjs.org/configuration-options/
     rollupOptions: {
       external: ['obsidian'],
+      output: {
+        assetFileNames(asset) {
+          if (asset.name === 'style.css') return 'styles.css';
+          return asset.name!;
+        },
+      },
     },
   },
 });
